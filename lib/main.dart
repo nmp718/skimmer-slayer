d76423717227d6a7c1b153d5b30e6c660aac374d
JamesDavidUD <dfourc@udel.edu>
2022-10-08 19:42:01 -0400

    I TYPED A MESSAGE HERE

--- lib/main.dart
+++ lib/main.dart
@@ -57,7 +57,7 @@ class _MyHomePageState extends State<MyHomePage> {
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
-      _counter =  _counter + 3;
+      _counter = _counter + 3;
     });
   }
 
@@ -96,7 +96,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'You have pushed the button too much please stop:',
             ),
             Text(
               '$_counter',
